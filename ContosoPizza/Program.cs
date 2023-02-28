92b69863ac1646a65ad5117cc876bc81bd057e4e
Eduardo Verbinen <everbinen@gmail.com>
2023-02-28 19:53:45 -0300

    Add ContosoPizza Project

---
+++ ContosoPizza/Program.cs
@@ -0,0 +1,26 @@
+//vscode://vscode.github-authentication/did-authenticate?windowid=1&code=6e59e265c6dbbde2a007&state=37bc540f-d0e5-4a91-97c0-4ca5634e443e  - Git Token
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
